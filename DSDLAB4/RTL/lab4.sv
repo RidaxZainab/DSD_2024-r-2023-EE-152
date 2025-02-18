6bd79340ca89fd2f8930002c539853782c92191f
RidaxZainab <ridaxzainab@gmail.com>
2025-02-18 11:08:03 +0500

    Update lab4.sv

--- DSDLAB4/RTL/lab4.sv
+++ DSDLAB4/RTL/lab4.sv
@@ -24,9 +24,9 @@ module lab4(
     input logic [1:0] a, b,
     output logic r, g, b_led
     );
-    assign r = (a > b);
-    assign g = (a == b);
-    assign b_led = (a < b);
+     assign r = (~b[1] & (~b[0] | a[0])) | (a[1] & (a[0] | ~b[1] | ~b[0]));
+     assign g = (~a[1] & (b[0] | b[1] | ~a[0])) | (b[1] & (b[0] | ~a[0]));
+     assign b_led = (a[1] ^ b[1]) | (a[0] ^ b[0]);
     endmodule
 
 
